7f570cc02b745d6f844df060acc13b62e679743f
Roee FL <roeefl@gmail.com>
2020-04-05 02:29:43 +0300

    Player methods

--- src/schemas/Player.ts
+++ src/schemas/Player.ts
@@ -1,10 +1,24 @@
 import { type, Schema, ArraySchema, MapSchema } from '@colyseus/schema';
 import GameCard from '../schemas/GameCard';
 import DiceRoll from './DiceRoll';
+import Structure from './Structure';
+import buildingCosts, { BuildingCost } from '../buildingCosts';
+import { purchaseTypes, resourceCardTypes, PURCHASE_SETTLEMENT, initialAvailableLoot, initialAvailablResourceCounts } from '../manifest';
 
 interface PlayerOptions {
   nickname: string
   color: string
+};
+
+interface HasResources {
+  [key: string]: boolean
+};
+
+const initialHasResources: HasResources = {
+  road: false,
+  settlement: false,
+  city: false,
+  gameCard: false
 };
 
 // Four sets of wooden player pieces in four different colors
@@ -37,11 +51,17 @@ class Player extends Schema {
   @type("number")
   roads: number = 15;
 
-  @type({ map: "number" })
-  resourceCounts = new MapSchema<Number>();
-
   @type([GameCard])
   gameCards: GameCard[];
+
+  @type({ map: "number" })
+  resourceCounts: MapSchema<Number>
+
+  @type({ map: "number" })
+  availableLoot: MapSchema<Number>
+
+  @type({ map: "boolean" })
+  hasResources: MapSchema<Boolean>
 
   @type("boolean")
   hasLongestRoad: boolean = false;
@@ -49,28 +69,111 @@ class Player extends Schema {
   @type("boolean")
   hasLargestArmy: boolean = false;
 
-  constructor(sessionId: string, options: PlayerOptions) {
+  @type(Structure)
+  lastStructureBuilt: Structure | null = null;
+
+  constructor(sessionId: string, options: PlayerOptions, color: string) {
     super();
     
-    const {
-      nickname = 'John Doe',
-      color = '#214013'
-    } = options;
+    const { nickname = 'John Doe' } = options;
+      // Disabled until enabling color distinction between players
+      // color = '#214013'
+    // } = options;
+    
+    const randomInt = Math.floor(Math.random() * 9999);
+    this.nickname = `${nickname} ${randomInt}`;
 
     this.playerSessionId = sessionId;
     this.isConnected = true;
-    this.nickname = nickname;
-    this.color = color;
     this.gameCards = new ArraySchema<GameCard>();
     this.rolls = new ArraySchema<DiceRoll>();
 
+    this.color = color;
+
     this.resourceCounts = new MapSchema<Number>({
-      lumber: 2,
-      sheep: 2,
-      brick: 2,
-      wheat: 2,
-      ore: 2
+      ...initialAvailablResourceCounts
     });
+
+    this.availableLoot = new MapSchema<Number>({
+      ...initialAvailableLoot
+    });
+
+    this.hasResources = new MapSchema<Boolean>(initialHasResources);
+  }
+
+  initializeSetupPhase() {
+    this.hasResources = new MapSchema<Boolean>({
+      road: false,
+      settlement: true,
+      city: false,
+      gameCard: false
+    });
+  }
+
+  onPurchase(type: string, isSetupPhase: boolean = false, isEndSetupPhase: boolean = false) {
+    if (isSetupPhase) {
+      if (type === PURCHASE_SETTLEMENT) {
+        this.hasResources = new MapSchema<Boolean>({
+          road: true,
+          settlement: false,
+          city: false,
+          gameCard: false
+        });
+
+        return;
+      }
+
+      this.hasResources = new MapSchema<Boolean>(initialHasResources);
+      return;
+    };
+
+    if (!isEndSetupPhase) {
+      const costs: BuildingCost = buildingCosts[type];
+
+      const updatedResourceCounts = resourceCardTypes.reduce((acc, name) => {
+        acc[name] = this.resourceCounts[name] - costs[name];
+        return acc;
+      }, {} as BuildingCost);
+
+      this.resourceCounts = new MapSchema<Number>(updatedResourceCounts);
+    }
+
+    const updatedHasResources = purchaseTypes.reduce((acc, purchaseType) => {
+      acc[purchaseType] = Object
+        .entries(this.resourceCounts)
+        .every(([resource, value]) => value >= buildingCosts[purchaseType][resource])
+
+      return acc;
+    }, {} as HasResources);
+
+    this.hasResources = new MapSchema<Boolean>(updatedHasResources);
+  }
+
+  saveLastStructure(structure: Structure) {
+    this.lastStructureBuilt = new Structure(this.playerSessionId, structure.type, structure.row, structure.col);
+  }
+
+  giveInitialResources() {
+    if (!this.lastStructureBuilt) return;
+    const { type, row, col } = this.lastStructureBuilt;
+
+    this.resourceCounts = new MapSchema<Number>({
+      lumber: 3,
+      sheep: 3,
+      brick: 3,
+      wheat: 3,
+      ore: 3
+    });
+
+    this.onPurchase('NONE', false, true);
+  }
+
+  giveResources(allStructures: Structure[], dice: Number[]) {
+    allStructures
+      .filter(({ ownerId }) => ownerId === this.playerSessionId)
+      .forEach(({ type, row, col }) => {
+
+      });
   }
 };
 
