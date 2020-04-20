c47892d857c362cbb0f15b235be48f24322a6604
Roee FL <roeefl@gmail.com>
2020-04-20 23:47:51 +0300

    Add clans api route

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,34 @@
 import express, { Application } from 'express';
 import buildingCosts from './specs/buildingCosts';
+import { clansManifest } from './specs/wildlings';
 
 export function setApiRoutes(app: Application) {
   // const api = express.Router();
+
+  function onError(res: express.Response, err: any) {
+    const { message } = err;
+    console.error(message);
+    
+    res.status(500);
+    res.json({ message });
+  }
 
   app.get('/api/buildingCosts', async (req: express.Request, res: express.Response) => {
     try {
       res.json({ buildingCosts }); 
     } catch (err) {
-      const { message } = err;
-      console.error(message);
-      
-      res.status(500);
-      res.json({ message });
+      onError(res, err);
     }
   });
+
+  app.get('/api/clans', async (req: express.Request, res: express.Response) => {
+    try {
+      res.json({ clans: clansManifest }); 
+    } catch (err) {
+      onError(res, err);
+    }
+  });
+
 
   // return api;
 };
