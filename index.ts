19ed0cafdfe75f7bfe9c9785445d892e756a5e1d
Roee FL <roeefl@gmail.com>
2020-04-29 12:36:55 +0300

    Refactor index.ts to match colyseus-examples

--- index.ts
+++ index.ts
@@ -1,49 +1,52 @@
-const cors = require('cors');
-require('dotenv').config();
 
 import express, { Application, Request, Response, Router } from 'express';
 import path from 'path';
+import cors from 'cors';
 import { createServer } from 'http';
 import { Server } from 'colyseus';
+import { monitor } from '@colyseus/monitor';
 import BaseGame from './src/rooms/BaseGame';
 import FirstMenGame from './src/rooms/FirstMenGame';
 import { ROOM_TYPE_BASE_GAME, ROOM_TYPE_FIRST_MEN } from './src/specs/roomTypes';
 import { setApiRoutes } from './src/api';
 
+require('dotenv').config();
+
 const port = Number(process.env.SERVER_PORT || 1337);
 
 const app: Application = express();
-app.use(express.json());
 app.use(cors());
-
-const router: Router = express.Router();
-
-app.get('/', function(req: Request, res: Response) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.json());
 
 setApiRoutes(app);
 
+// const router: Router = express.Router();
+
 const server = createServer(app);
 const gameServer = new Server({
-  server
+  server,
+  express: app
 });
 
 gameServer.define(ROOM_TYPE_BASE_GAME, BaseGame);
 gameServer.define(ROOM_TYPE_FIRST_MEN, FirstMenGame);
 
-gameServer.onShutdown(() => {
-  console.info('game server is going down.');
-});
-
-// app.use('/colyseus', monitor());
 // router.use('/', express.static(monitorPath));
+app.use('/colyseus', monitor());
+
+app.get('/', function(req: Request, res: Response) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 const monitorPath = path.join(__dirname + '/src/monitor/dist/index.html')
 app.get('/monitor', function(req: Request, res: Response) {
   res.sendFile(monitorPath);
 });
 
+gameServer.onShutdown(() => {
+  console.info('game server is going down.');
+});
 gameServer.listen(port);
+
 const serverUrl = process.env.SERVER_URL || 'ws://localhost';
 console.info(`Server Started at: ${serverUrl}`);
