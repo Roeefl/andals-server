4d1136fb9af52f723d1992a389dc9cd8dcb1c8dc
Roee FL <roeefl@gmail.com>
2020-05-03 16:26:56 +0300

    HeroCards | move title, ability, description from each hero card from server-side to client-side to avoid unnecessary data bandwidth

--- src/schemas/HeroCard.ts
+++ src/schemas/HeroCard.ts
@@ -3,9 +3,6 @@ import { type, Schema } from '@colyseus/schema';
 interface HeroCardManifest {
   order?: number
   name: string
-  title: string
-  ability?: string
-  description?: string
   isReady?: boolean
 };
 
@@ -13,120 +10,72 @@ const manifest: { [type: string] : HeroCardManifest } = {
   JeorMormont: {
     isReady: true,
     order: 1,
-    name: "Jeor Mormont",
-    title: "Lord Commander of the Night's Watch",
-    ability: "Forced Trade",
-    description: "Demand 1 resource card each from up to 2 players. The cards must be of the same resource type. For each resource card you receive, give the respective player 1 resource card of your choice in return."
+    name: "Jeor Mormont"
   },
   BowenMarsh: {
     isReady: true,
     order: 2,
-    name: "Bowen Marsh",
-    title: "Lord Steward of the Night's Watch",
-    ability: "Fair Trade",
-    description: "You may trade a single resource card at a rate of 1:1 with the supply." 
+    name: "Bowen Marsh"
   },
   SamwellTarly: {
     order: 3,
     isReady: true,
-    name: "Samwell Tarly",
-    title: "Steward",
-    ability: "Master's Sufficiency",
-    description: "If, after resolving any production roll (not a “7”), you receive no resource cards, take any 1 resource card of your choice from the supply. Take your resource card before any other hero card is used."
+    name: "Samwell Tarly"
   },
   OthellYarwyck: {
     isReady: true,
     order: 4,
-    name: "Othell Yarwyck",
-    title: "Master Builder of the Night's Watch",
-    ability: "Road Redirect",
-    description: "Remove 1 of your roads from the board and rebuild it for free at a different legal location. The road you remove must only be connected on 1 of its 2 ends to your pieces. Ignore opponents’ pieces when checking connections."
+    name: "Othell Yarwyck"
   },
   AlliserThorne: {
     isReady: true,
     order: 5,
-    name: "Ser Alliser Thorne",
-    title: "Master at Arms of the Night's Watch",
-    ability: "Easy to Build Guard",
-    description: "When you build a guard, substitute 1 of the 3 resources with any 1 other resource of your choice."
+    name: "Ser Alliser Thorne"
   },
   Melisandre: {
     order: 6,
     isReady: true,
-    name: "Melisandre",
-    title: "Red Priestess of R'hllor",
-    ability: "Manipulate Development Cards",
-    description: "One time on your turn, when you buy a development card, substitute 1 of the 3 resources with any 1 other resource of your choice. Choose your development card from the 3 top cards of the deck, then reshuffle the deck."
+    name: "Melisandre"
   },
   ManceRayder: {
     isReady: true,
-    name: "Mance Rayder",
-    title: "King Beyond the Wall",
-    ability: "Underdog",
-    description: "After resolving your production roll, look at the hand of resource cards of 1 opponent who has more victory points than you and take 1 resource card of your choice."
+    name: "Mance Rayder"
   },
   BenjenStark: {
     isReady: true,
-    name: "Benjen Stark",
-    title: "First Ranger",
-    ability: "Coldhands",
-    description: "Remove 1 wildling from a clearing or a camp, and return it to the spawn. Collapse the camps if needed. Then, reveal a wildling token from the pool."
+    name: "Benjen Stark"
   },
   Ygritte: {
     isReady: true,
-    name: "Ygritte",
-    title: "Freefolk Spearwife",
-    ability: "Back to the Cave",
-    description: "Choose 1 wildling in the Gift and return it to the Frostfangs. You can do this before or after your own production roll."
+    name: "Ygritte"
   },
   Yoren: {
     isReady: true,
-    name: "Yoren",
-    title: "Wandering Crow",
-    ability: "New Recruits",
-    description: "When you build, discard 1 Ranger Patrol card that you have already played and build a guard for free. You can do this before or after your own production roll."
+    name: "Yoren"
   },
   QhorinHalfhand: {
     isReady: true,
-    name: "Qhorin Half-Hand",
-    title: "Ranger",
-    ability: "Fist of the First Men",
-    description: "You may move Tormund back to his camp. You receive 1 resource of the type produced by the land that Tormund left. You can do this before or after your own production roll."
+    name: "Qhorin Half-Hand"
   },
   IlynPayne: {
     isReady: true,
-    name: "Ilyn Payne",
-    title: "The Queen's Justice",
-    ability: "Guard Execution",
-    description: "You may immediately remove any Guard on the wall which belongs to another player, and return it to him."
+    name: "Ilyn Payne"
   },
   EuronGreyjoy: {
     isReady: true,
-    name: "Euron Greyjoy",
-    title: "King of Salt and Rock",
-    ability: "Iron Fleet Occupation",
-    description: "You may trade any resource cards with the supply at a rate of 2:1, as if you had a settlement on each of the harbor trading posts."
+    name: "Euron Greyjoy"
   },
   TywinLannister: {
     isReady: true,
-    name: "Tywin Lannister",
-    title: "Lord of Casterly Rock",
-    ability: "A lion does not concern himself with the Sheep",
-    description: "Trade any of your resource cards with the supply for one sheep resource card."
+    name: "Tywin Lannister"
   },
   Thoros: {
     isReady: true,
-    name: "Thoros of Myr",
-    title: "Priest of R'hollor",
-    ability: "The one True God",
-    description: "Whenever one of your guards gets killed during an attack on the wall, instantly revive him and place him back at any wall section."
+    name: "Thoros of Myr"
   },
   StannisBaratheon: {
     isReady: true,
-    name: "Stannis Baratheon",
-    title: "Azor Ahai",
-    ability: "March to the Wall",
-    description: "You may immediately move one of your guards to any wall section."
+    name: "Stannis Baratheon"
   },
   // Littlefinger: {
   //   name: "Petyr Baelish",
@@ -137,8 +86,7 @@ const manifest: { [type: string] : HeroCardManifest } = {
   // DavosSeaworth: {
   //   name: "Davos Seaworth",
   //   title: "Admiral of the Narrow Sea",
-  //   ability: "",
-  //   description: ""
+  //   ability: ""
   // },
   // RobbStark: {
   //   name: "Robb Stark",
@@ -179,22 +127,13 @@ export const [
 class HeroCard extends Schema {
   @type("string")
   type: string;
-
+  
   @type("string")
   name: string;
-
+  
   @type("number")
   order: number
-
-  @type("string")
-  title: string
-
-  @type("string")
-  ability: string
-
-  @type("string")
-  description: string
-
+  
   @type("boolean")
   wasPlayed: boolean = false
 
@@ -207,9 +146,6 @@ class HeroCard extends Schema {
     this.type = type;
     this.order = manifest[type].order || 0;
     this.name = manifest[type].name;
-    this.title = manifest[type].title;
-    this.ability = manifest[type].ability || '';
-    this.description = manifest[type].description || '';
     this.ownerId = null;
   }
 
